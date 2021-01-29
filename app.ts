b6eaa755b9be01a4898c58cae05964210c227180
ramtin heidarrezaei <ramtin.hrezaei@gmail.com>
2021-01-29 10:49:23 +0330

    undrestand union and literal types

--- app.ts
+++ app.ts
@@ -1,14 +1,30 @@
-function combine(input1: number | string, input2: number | string) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text',
+) {
+  let result
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2
   } else {
-    result = input1.toString() + input2.toString();
+    result = input1.toString() + input2.toString()
   }
-  return result;
+  return result
+  // if (resultConversion === 'as-number') {
+  //   return +result
+  // } else {
+  //   return result.toString()
+  // }
 }
 
-const combinedAges = combine(30, 26);
-console.log(combinedAges);
+const combinedAges = combine(30, 26, 'as-number')
+console.log(combinedAges)
 
-const combinedNames = combine("Max", "Ramtin");
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Max', 'Ramtin', 'as-text')
+console.log(combinedNames)
--- objs-arrays-enums.ts
+++ objs-arrays-enums.ts
@@ -22,27 +22,27 @@ enum Role {
   ADMIN = 'Admin',
   READ_ONLY = 1,
   AUTHOR = 2,
-};
+}
 
 const person = {
   name: 'ramtin',
   age: 21,
   hobbies: ['Sports', 'Coding'],
   role: Role.ADMIN,
-};
+}
 
 // person.role.push('admin');
 // person.role[0] = 10;
 
-let favriteActivities: string[];
-favriteActivities = ['Sports'];
+let favriteActivities: string[]
+favriteActivities = ['Sports']
 
-console.log(person.name);
+console.log(person.name)
 
 for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+  console.log(hobby.toUpperCase())
 }
 
 if (person.role === Role.ADMIN) {
-  console.log('hi admin');
+  console.log('hi admin')
 }
